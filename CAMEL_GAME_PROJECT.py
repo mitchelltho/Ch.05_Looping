b64d015e6218ecbc063456ef4075a59a2196aefe
mitchelltho <mitchelltho@urbandaleschools.com>
2023-10-20 08:59:31 -0500

    Sussy commit message

--- CAMEL_GAME_PROJECT.py
+++ CAMEL_GAME_PROJECT.py
@@ -4,6 +4,46 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+import random
+milestraveled = 0
+thirst = 0
+cameltiredness = 0
+nativedistance = -20
+canteen = 4
+done = bool(False)
+print()
+
+while done == bool(False):
+    nativedistance = nativedistance - milestraveled
+    print("A. Drink from your canteen.")
+    print("B. Ahead moderate speed.")
+    print("C. Ahead full speed.")
+    print("D. Stop for the night.")
+    print("E. Status check.")
+    print("Q. Quit.")
+    userchoice = input("What do you choose? ").upper()
+    if userchoice == "Q":
+        done = bool(True)
+    elif userchoice == "A":
+        print("test")
+    elif userchoice == "B":
+        print("test")
+    elif userchoice == "C":
+        milestraveled += random.randint(9,20)
+        print("You have traveled " + str(milestraveled) + " Miles")
+        thirst += 1
+        cameltiredness += random.randint(0,3)
+        nativedistance += random.randint(7,14)
+    elif userchoice == "D":
+        cameltiredness = 0
+        print("Your camel is happy")
+        nativedistance += random.randint(7,14)
+    elif userchoice == "E":
+        print("Miles Traveled: " + str(milestraveled))
+        print("Drinks in canteen: " + str(canteen))
+        print("The Natives Are " + str(abs(nativedistance)) + " Miles Behind You")
+    else:
+        continue
 
 
 
